2053831cb65bb94424e982dc0d3ae7107d7fb7fd
daniels290813 <78727943+daniels290813@users.noreply.github.com>
2025-06-23 11:35:12 +0300

    Update project_setup.py

--- project_setup.py
+++ project_setup.py
@@ -24,11 +24,11 @@ def setup(
     
     
     project.set_source("git://github.com/daniels290813/jiggy.git#main", pull_at_runtime=False)
-    pipeline_fn = project.set_function('untitled.py',name='hello', kind='job', image='mlrun/mlrun-kfp', handler='hello')
+    pipeline_fn = project.set_function('untitled.py',name='hello', kind='job', image='mlrun/mlrun', handler='hello')
     pipeline_fn.save()
     project.set_function(f"db://{project.name}/hello", name="hello")
     project.save()
-    pipe_image = project.build_image(base_image='mlrun/mlrun', set_as_default=False)
+    pipe_image = project.build_image(base_image='mlrun/mlrun-kfp', set_as_default=False)
     print(pipe_image.outputs)
     project.set_workflow("my_workflow", "pipeline.py", embed=True, image=pipe_image.outputs['image'])
     
